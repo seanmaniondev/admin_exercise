352fce5e961de3b70d466893469f9ff8cae9c8c7
jnappy <jpnappy@gmail.com>
2016-03-06 12:05:32 -0500

    Update admin.rb

--- admin.rb
+++ admin.rb
@@ -8,15 +8,15 @@ require 'pry'
 
 #write a function "user_permission" that accepts four parameters: signed_in, admin, paid, and cancelled
 #if the user doesn't pay their bills or has canceled a deal, show "go away!"
-#if the user is signed in and they are an admin, show "you can see see and change all the pages!"
-#if you user is signed in but they are not an admin, show "you can see all the pages!"
+#if the user is signed in and they are an admin, show "you can see and change all the pages!"
+#if the user is signed in but they are not an admin, show "you can see all the pages!"
 #if the user is not signed in, show "you can't see any of the pages, please sign in!"
 
 #ask the user if they pay their bills (yes/no)
 
 #store their answer in a variable
 
-#ask the user if they have canceled a deal (yes/no)
+#ask the user if they have cancelled a deal (yes/no)
 
 #store their answer in a variable
 
